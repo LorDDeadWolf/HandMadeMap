11f09c4f7771f96d2b7897b9bc00c6d5ecbaba3d
Mikhail Zaitsau <mikhail_zaitsau@outlook.com>
2022-11-13 00:03:23 +0300

    It is work!

--- HandMadeMap.rb
+++ HandMadeMap.rb
@@ -1,5 +1,14 @@
 class Array
-    def  hmmap(&block)
-    
+    def  hmmap
+        arr = [] #создается пустой массив
+        
+        return "#<Enumerator: ...>" unless block_given? #проверка на наличие блока
+        
+        for i in self #обработка каждого элемента массива заднным блоком и добавление массив
+            arr << yield(i)
+        end
+        
+        arr #выводится масиив после обработки
     end 
 end
+
